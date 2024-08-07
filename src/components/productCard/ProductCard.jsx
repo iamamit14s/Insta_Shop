269073eb42c7deea68dc79ed99bd83ea470b6062
Amit Sinha <s14saurabh@gmail.com>
2024-08-08 04:49:03 +0530

    check

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -37,65 +37,63 @@ function ProductCard() {
         </div>
 
         <div className="flex flex-wrap -m-4">
-          {product
-            .slice(0, 8)
-            .map((item, index) => {
-              const { title, price, description, imageURL, id } = item;
-              return (
-                <div key={index} className="p-4 md:w-1/4  drop-shadow-lg ">
+          {product.slice(0, 8).map((item, index) => {
+            const { title, price, description, imageURL, id } = item;
+            return (
+              <div key={index} className="p-4 md:w-1/4  drop-shadow-lg ">
+                <div
+                  className="relative h-full flex flex-col border-2 hover:shadow-pink-700 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
+                  style={{
+                    backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
+                    color: mode === "dark" ? "white" : "",
+                  }}
+                >
                   <div
-                    className="relative h-full flex flex-col border-2 hover:shadow-pink-700 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden"
-                    style={{
-                      backgroundColor: mode === "dark" ? "rgb(46 49 55)" : "",
-                      color: mode === "dark" ? "white" : "",
-                    }}
+                    onClick={() =>
+                      (window.location.href = `/productinfo/${id}`)
+                    }
+                    className="flex justify-center cursor-pointer"
                   >
-                    <div
-                      onClick={() =>
-                        (window.location.href = `/productinfo/${id}`)
-                      }
-                      className="flex justify-center cursor-pointer"
+                    <img
+                      className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out"
+                      src={imageURL}
+                      alt="blog"
+                    />
+                  </div>
+                  <div className="p-5 flex flex-col flex-grow border-t-2">
+                    <h2
+                      className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                      style={{ color: mode === "dark" ? "white" : "" }}
                     >
-                      <img
-                        className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out"
-                        src={imageURL}
-                        alt="blog"
-                      />
-                    </div>
-                    <div className="p-5 flex flex-col flex-grow border-t-2">
-                      <h2
-                        className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-                        style={{ color: mode === "dark" ? "white" : "" }}
+                      INSTA SHOP
+                    </h2>
+                    <h1
+                      className="title-font text-lg font-medium text-gray-900 mb-3"
+                      style={{ color: mode === "dark" ? "white" : "" }}
+                    >
+                      {title}
+                    </h1>
+                    {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
+                    <p
+                      className="leading-relaxed mb-3"
+                      style={{ color: mode === "dark" ? "white" : "" }}
+                    >
+                      ₹ {price}
+                    </p>
+                    <div className="mt-auto flex justify-center">
+                      <button
+                        onClick={() => addCart(item)}
+                        type="button"
+                        className=" focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2"
                       >
-                        INSTA SHOP
-                      </h2>
-                      <h1
-                        className="title-font text-lg font-medium text-gray-900 mb-3"
-                        style={{ color: mode === "dark" ? "white" : "" }}
-                      >
-                        {title}
-                      </h1>
-                      {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
-                      <p
-                        className="leading-relaxed mb-3"
-                        style={{ color: mode === "dark" ? "white" : "" }}
-                      >
-                        ₹ {price}
-                      </p>
-                      <div className="mt-auto flex justify-center">
-                        <button
-                          onClick={() => addCart(item)}
-                          type="button"
-                          className=" focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2"
-                        >
-                          Add To Cart
-                        </button>
-                      </div>
+                        Add To Cart
+                      </button>
                     </div>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            );
+          })}
         </div>
         {/* <div className=" flex justify-center">
                                             <button onClick={()=>addCart()} type="button" className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2">Add To Cart</button>
