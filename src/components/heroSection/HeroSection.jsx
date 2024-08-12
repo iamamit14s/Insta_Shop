8798ca03d4b81efa008bbe20a8dcf818300a9585
Amit Sinha <s14saurabh@gmail.com>
2024-08-13 01:17:28 +0530

    u-hero image

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img src="../img/hero.png"  alt="" />
+        <img src="../img/hero2.webp"  width={1350} alt="" />
     </div>
   )
 }
